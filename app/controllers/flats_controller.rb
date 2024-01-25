9637774f72377f49edbd8ef9e8871d8f4649f7c3
juan bernal <mesieou@gmail.com>
2024-01-25 16:22:45 +1100

    styled the form

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -6,4 +6,19 @@ class FlatsController < ApplicationController
   def new
     @flat = Flat.new
   end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :price_per_night, :address, :number_of_guests, :description)
+  end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,4 @@
 class Flat < ApplicationRecord
+
+  validates :name, :price_per_night, :address, :number_of_guests, :description, presence: true
 end
