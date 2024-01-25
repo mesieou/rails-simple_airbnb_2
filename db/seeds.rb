71c64f6dc26a75f8530a390071988fde4ab69609
juan bernal <mesieou@gmail.com>
2024-01-25 11:09:13 +1100

    flat model and seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Destroying all flats'
+Flat.destroy_all
+puts 'Creating flats'
+4.times do
+  flat = Flat.create!(
+  name: Faker::Superhero.name,
+  address: Faker::Address.street_address,
+  description:'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: (20..200).to_a.sample,
+  number_of_guests: (1..20).to_a.sample
+  )
+  puts "#{flat.name} created!"
+end
+puts 'All finished'
